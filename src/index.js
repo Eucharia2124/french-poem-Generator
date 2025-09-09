e3dc6ec1b3680194fe7aef6ed50f7de05a686ba2
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-09-09 20:50:59 +0200

    improve the users experience

--- src/index.js
+++ src/index.js
@@ -16,7 +16,12 @@ function generatePoem(event) {
     let prompt = `User instructions: Generate a short poem in French about ${instructionInput.value}`;
     let context = "You are a romantin poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separete each line with a <br />. Make sure the poem in basic HTML. make sure to follow the user`s instructions. Do not include a title to the poem, Sign the poem with `SheCodes AI` inside a <strong> element";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-   
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "generating"> Generating a French poem about ${instructionInput.value}</div>`;
+
+
    console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
